a53971f413c4b91cc1dd69e8283b8db2fddd60e7
DeerDancer5 <DeerDancer5@github.com>
2023-05-16 23:46:18 +0200

    Utworzenie projektu

---
+++ src/main/java/com/projekt/projekt/UserController.java
@@ -0,0 +1,22 @@
+package com.projekt.projekt;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class UserController {
+    @GetMapping
+    @RequestMapping("/")
+    String getUser(Model model) {
+        model.addAttribute("Name","UserName");
+        ArrayList<String> notes = new ArrayList<>();
+        notes.add("Note1");
+        notes.add("Note2");
+        notes.add("Note3");
+        model.addAttribute("Notes",notes);
+        return "user";
+    }
+}
---
+++ src/test/java/com/projekt/projekt/ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
