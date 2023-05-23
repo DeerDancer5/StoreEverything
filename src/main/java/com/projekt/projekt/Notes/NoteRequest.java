17d8ebb7e1a82e8485ce269c88574e20105a0942
DeerDancer5 <DeerDancer5@github.com>
2023-05-23 19:37:22 +0200

    Zmiany w formularzach i stworzenie klasy NoteRequest

---
+++ src/main/java/com/projekt/projekt/Notes/NoteRequest.java
@@ -0,0 +1,31 @@
+package com.projekt.projekt.Notes;
+
+public class NoteRequest {
+    private String sortBy;
+    private String pageSize;
+    private String page;
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public String getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(String pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+}
