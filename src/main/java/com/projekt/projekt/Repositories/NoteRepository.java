c6e1ca88e59755eb2299b8388c036f4242ae7be1
DeerDancer5 <80452@student.pb.edu.pl>
2023-07-20 23:42:16 +0200

    Poprawki wyświetlania kategorii dla nowego użytkownika

--- src/main/java/com/projekt/projekt/Repositories/NoteRepository.java
+++ src/main/java/com/projekt/projekt/Repositories/NoteRepository.java
@@ -1,17 +1,12 @@
 package com.projekt.projekt.Repositories;
-
 import com.projekt.projekt.Notes.Note;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
